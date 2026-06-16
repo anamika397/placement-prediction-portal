6527b8395898599748aae253aa1efe2f3c3e9c1f
Anamika <anamikagupta37139@gmail.com>
2026-06-16 15:10:54 +0530

    updated student profile UI and improved registration

--- students/forms.py
+++ students/forms.py
@@ -19,7 +19,32 @@ class StudentRegistrationForm(forms.ModelForm):
             'communication',
             'projects'
         ]
-
+widgets = { 
+    'name': forms.TextInput(attrs={ 
+        'class': 'form-control', 
+        'placeholder': 'Enter full name' }),
+          'email': forms.EmailInput(attrs={ 
+              'class': 'form-control', 
+              'placeholder': 'Enter email' }), 
+              'password': forms.PasswordInput(attrs={ 
+                  'class': 'form-control',
+                    'placeholder': 'Create password' }),
+                      'branch': forms.TextInput(attrs={
+                           'class': 'form-control',
+                             'placeholder': 'Branch' }), 
+                             'semester': forms.NumberInput(attrs={ 
+                                 'class': 'form-control' }), 
+                                 'cgpa': forms.NumberInput(attrs={ 
+                                     'class': 'form-control' }), 
+                                     'aptitude': forms.NumberInput(attrs={
+                                          'class': 'form-control' }),
+                                            'coding': forms.NumberInput(attrs={ 
+                                                'class': 'form-control' }),
+                                                  'communication': forms.NumberInput(attrs={ 
+                                                      'class': 'form-control' }), 
+                                                      'projects': forms.NumberInput(attrs={
+                                                           'class': 'form-control' }), 
+                                                           }
 
 class StudentProfileForm(forms.ModelForm):
 
@@ -35,6 +60,44 @@ class StudentProfileForm(forms.ModelForm):
             'communication',
             'projects'
         ]
+
+        widgets = {
+
+            'branch': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter branch'
+            }),
+
+            'semester': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter semester'
+            }),
+
+            'cgpa': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter CGPA'
+            }),
+
+            'aptitude': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Aptitude score'
+            }),
+
+            'coding': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Coding score'
+            }),
+
+            'communication': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Communication score'
+            }),
+
+            'projects': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Number of projects'
+            }),
+        }
 
 class PredictionForm(forms.Form):
 
